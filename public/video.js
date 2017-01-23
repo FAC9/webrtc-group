8c1bd59fcc96e342812203876766abfdb12b17d2
jsms90 <jsms@hotmail.co.uk>
2017-01-23 17:01:03 +0000

    add register & send functions

--- public/video.js
+++ public/video.js
@@ -15,3 +15,41 @@ promise.then((avStream) => {
     video.srcObject = avStream;
     video.play();
    });
+
+var directory = {
+  // jen: sendMessage,
+  // nori: sendMessage
+};
+
+var registerUser = function (from, callback) {
+  if (Object.keys(directory).indexOf(from) === -1) {
+    directory[from] = callback;
+  }
+}
+
+onEventListenerOfSomeKind = registerUser("jen", sendMessage);
+onEventListenerOfSomeKind = registerUser("nori", sendMessage);
+
+var sendMessage = function (friend, connectionType, data) {
+  if (directory.hasOwnProperty(friend)) {
+    var target = directory[friend];
+
+  }
+
+
+  }
+
+}
+
+
+
+signalingChannel.onmessage = function (evt) {
+   if (!pc)
+       start(false);
+
+   var signal = JSON.parse(evt.data);
+   if (signal.sdp)
+       pc.setRemoteDescription(new RTCSessionDescription(signal.sdp));
+   else
+       pc.addIceCandidate(new RTCIceCandidate(signal.candidate));
+};
